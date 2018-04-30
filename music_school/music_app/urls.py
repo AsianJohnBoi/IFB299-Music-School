ae8fc182804aa3997502c02395e01670c4efc749
michaelbell97 <m21.bell@connect.qut.edu.au>
2018-04-30 12:03:10 +1000

    changed a heap of css to try match client taste

--- music_school/music_app/urls.py
+++ music_school/music_app/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('signup/', views.SignUp, name='signup'),
     path('account/edit/', views.edit_account, name='edit_account'),
+
 ]
